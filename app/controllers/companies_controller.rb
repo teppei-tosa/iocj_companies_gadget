d76299fe7bea750e226f21027a9cddbe5501ee4b
土佐鉄平 <tosa@tosateppei-no-MacBook-Pro.local>
2015-11-23 00:02:40 +0900

    sqlite3を入れ直してうまくいったので、ガジェットXMLを応答するように修正

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -1,6 +1,6 @@
 class CompaniesController < ApplicationController
   def index
-    @hoge = 'hoge'
+    @companies = {:title => 'IOCJ Companies', :description => 'Here is IOCJ mambers!', :msg => 'hoge!'}
     respond_to do |format|
       format.xml
     end
