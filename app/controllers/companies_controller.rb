ab543d7dfea55d5012aade580b56b86826f37fa9
土佐鉄平 <tosa@tosateppei-no-MacBook-Pro.local>
2015-11-23 00:13:39 +0900

    infoscoopからはhtmlのリクエストになるようなので、htmlとしてガジェットを応答するようにする

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -2,7 +2,7 @@ class CompaniesController < ApplicationController
   def index
     @companies = {:title => 'IOCJ Companies', :description => 'Here is IOCJ mambers!', :msg => 'hoge!'}
     respond_to do |format|
-      format.xml
+      format.html
     end
   end
 end
