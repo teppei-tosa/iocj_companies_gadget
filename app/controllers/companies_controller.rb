b799bd7c61a31765537ef8516320339c81162830
土佐鉄平 <tosa@tosateppei-no-MacBook-Pro.local>
2015-11-23 14:48:08 +0900

    IOCJ サイトからのスクレイピング処理を実装

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -1,6 +1,25 @@
+require 'open-uri'
+require 'nokogiri'
+
 class CompaniesController < ApplicationController
   def index
-    @companies = {:title => 'IOCJ Companies', :description => 'Here is IOCJ mambers!', :msg => 'hoge!'}
+
+    url = 'https://www.infoscoop.org/iocj/member-list/'
+
+    charset = nil
+    html = open(url) do |f|
+      charset = f.charset # 文字種別を取得
+      f.read # htmlを読み込んで変数htmlに渡す
+    end
+
+    # htmlをパース(解析)してオブジェクトを生成
+    doc = Nokogiri::HTML.parse(html, nil, charset)
+
+    @companies = []
+    doc.css('.member-list').css('li').each do |company|
+      @companies.push(company.inner_text)
+    end
+
     respond_to do |format|
       format.html
     end
